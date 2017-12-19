4aa372d0401b8fec3b968b78cfb154e4ce2d86aa
unknown <aaronedelman@yahoo.com>
2017-12-19 14:08:47 -0600

    added TODO

---
+++ HumaneSociety/HumaneSociety/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//Database: Animal type, animal cost, animal room, adopted?, shots administered?, food amount
+
+//Adopter: 
+//    Create Profile: name, age, housing status, other pets, kids.
+//    Search DB (multiple criteria) for potential pets.
+//    Ability to pay for animal.
+
+//Employee: Import CSV file
+//        Add animal to DB.
+//        Change & view animal status, room, shots, etc.
+//        Give animal shots
+//        Collect money from adopter
